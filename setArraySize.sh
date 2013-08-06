f81a3e9e50350a94471b6ec0cd19be7eed669f26
bt- <benjaming.taylor@gmail.com>
2013-08-05 21:38:18 -0500

    Added GPL v2 license

--- setArraySize.sh
+++ setArraySize.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+#	LICENSE:
+#    Script to run parametric EnergyPlus solar thermal simulations.
+#    Copyright (C) 2013 Benjamin G. Taylor
+#
+#    This program is free software; you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation; either version 2 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License along
+#    with this program; if not, write to the Free Software Foundation, Inc.,
+#    51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+
+
 #========= FILE FOR SETTING MULTIPLYING COLLECTORS TO NUMBER REQUIRED FOR ARRAY AREA ===
 # ARGUMENT 1 ====  E+ FILENAME WITH .idf EXTENSION
 # ARGUMENT 2 ====  ARRAY AREA
